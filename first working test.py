c1fb96c0d7136152ebd14b7376ec3dc310a1fe6d
Abhishek02bhardwaj <96164521+Abhishek02bhardwaj@users.noreply.github.com>
2023-03-09 21:37:50 +0530

    Add files via upload

---
+++ first working test.py
@@ -0,0 +1,59 @@
+import os
+import yaml
+import csv
+
+supported_pairs = []
+
+# Loop through all the YAML files in the config directory
+for filename in os.listdir("config"):
+    if filename.endswith(".yaml"):
+        with open(os.path.join("config", filename), "r") as f:
+            config = yaml.safe_load(f)
+
+            # Check if the YAML file has a "handler" key
+            if "handler" in config:
+                handler = config["handler"]
+                if handler == "Wikidata":
+                    # Handle Wikidata-based configuration files
+                    source_lang = config["source"]["from"]
+                    target_langs = [lang["to"] for lang in config["target"]]
+                    engine = "Wikidata"
+                    preferred_engine = False
+                elif handler == "Meta":
+                    # Handle Meta-based configuration files
+                    source_lang = filename.split(".")[0]
+                    target_langs = config["languages"]
+                    engine = config["engine"]
+                    preferred_engine = config.get("preferred_engine", False)
+                # else:
+                    raise ValueError("Unsupported handler: {handler}")
+            else:
+                # Handle standard configuration files
+                # Check if config is a list
+                if isinstance(config, list):
+                    # Handle list-based configuration files
+                    source_lang = filename.split(".")[0]
+                    target_langs = config
+                    engine = "Apertium"
+                    preferred_engine = False
+                else:
+                    # Handle dictionary-based configuration files
+                    source_lang = list(config.keys())[0]
+                    target_langs = config[source_lang]
+                    engine = config.get("engine", "Apertium")
+                    preferred_engine = config.get("preferred_engine", False)
+
+            # Add the supported pairs to the list
+            for target_lang in target_langs:
+                supported_pairs.append({
+                    "source language": source_lang,
+                    "target language": target_lang,
+                    "translation engine": engine,
+                    "is preferred engine?": preferred_engine
+                })
+
+# Export the list as a CSV file
+with open("supported_pairs.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=["source language", "target language", "translation engine", "is preferred engine?"])
+    writer.writeheader()
+    writer.writerows(supported_pairs)
