e282cdcf8d8e4402421d504f0f40e9687a8584e4
Abhishek Bhardwaj <abhishek02bhardwaj.er@gmail.com>
2023-03-19 12:24:07 +0530

    Used a dictionary to store mt-defaults data structure

--- final_updated.py
+++ final_updated.py
@@ -5,15 +5,19 @@ with open("supported_pairs.csv", "r") as f:
     reader = csv.DictReader(f)
     supported_pairs = [row for row in reader]
 
-# Read in the mt-defaults.csv file
+# Read in the mt-defaults.csv file and build a dictionary for O(1) lookups
 with open("mt-defaults.csv", "r") as f:
     reader = csv.DictReader(f)
-    mt_defaults = [row for row in reader]
+    mt_defaults = {}
+    for row in reader:
+        key = (row["source language"], row["target language"], row["translation engine"])
+        mt_defaults[key] = row
 
 # Loop over each pair in the supported_pairs list
 for pair in supported_pairs:
-    # Find the matching pair in the mt_defaults list
-    match = next((p for p in mt_defaults if p["source language"] == pair["source language"] and p["target language"] == pair["target language"] and p["translation engine"] == pair["translation engine"]), None)
+    # Look up the matching pair in the mt_defaults dictionary
+    key = (pair["source language"], pair["target language"], pair["translation engine"])
+    match = mt_defaults.get(key)
     # If a match is found, update the "is preferred engine?" flag to True
     if match:
         pair["is preferred engine?"] = "True"
